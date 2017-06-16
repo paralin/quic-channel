0d2dedc13e34c162550ea035af3d968ccef7f8c1
Christian Stewart <christian@paral.in>
2017-06-16 11:00:37 -0700

    refactor: tweaks to key code structure

--- identity/key.go
+++ identity/key.go
@@ -10,8 +10,14 @@ import (
 	"strings"
 )
 
+// ipv6AddrPrefix is the prefix for every IPV6 addr in the system.
+var ipv6AddrPrefix = []byte{0xfd, 0xcc}
+
 // PublicKeyHash is the hash of a public key.
 type PublicKeyHash [sha256.Size]byte
+
+// ClusterCertHash is the hash of the cluster CA cert public key.
+type ClusterCertHash [sha256.Size]byte
 
 // MarshalPublicKey encodes one or more public keys to pem format.
 func MarshalPublicKey(pkeys ...interface{}) ([]byte, error) {
@@ -50,20 +56,29 @@ func (h *PublicKeyHash) MarshalHashIdentifier() string {
 
 // ToIPv6Addr converts the public key hash to a IP address.
 func (h *PublicKeyHash) ToIPv6Addr(caCert *x509.Certificate) (net.IP, error) {
-	// Generate the subnet etc from the ca cert.
-	caHash, err := HashPublicKey(caCert.PublicKey)
+	caHash, err := HashCACertificate(caCert)
 	if err != nil {
 		return nil, err
 	}
 
 	ip := net.IP(make([]byte, 16))
 	// Prefix + L
-	ip[0] = 0xfd
-	ip[1] = 0xcc
+	copy(ip, ipv6AddrPrefix)
 	// Copy the first 6 bytes of the hash to the global ID and subnet ID.
 	copy(ip[2:], caHash[:6])
 	// Copy the rest from the hash
 	copy(ip[8:], (*h)[:8])
 
 	return ip, nil
+}
+
+// HashCACertificate returns a CaCertHash for the purposes of IPv6 address generation.
+func HashCACertificate(caCert *x509.Certificate) (*ClusterCertHash, error) {
+	// Generate the subnet etc from the ca cert.
+	caHash, err := HashPublicKey(caCert.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+	h := ClusterCertHash(*caHash)
+	return &h, nil
 }
